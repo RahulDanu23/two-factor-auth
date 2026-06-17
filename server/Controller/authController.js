995a585fb40df231252f68d39d995214f7a5c1fa
RahulDanu23 <danurahul23548@gmail.com>
2026-06-17 16:05:39 +0530

    Fix cross-origin cookie blocking

--- server/Controller/authController.js
+++ server/Controller/authController.js
@@ -78,8 +78,8 @@ export const login = async (req, res) => {
         
         res.cookie('tempToken', tempToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 60 * 60 * 1000 // 1 hour
         });
 
@@ -98,8 +98,8 @@ export const logout = async(req, res) => {
     try {
         res.clearCookie('token', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
         });
         return res.json({success:true, message:"Logout Success"});
     } catch (error) {
@@ -174,8 +174,8 @@ export const verifyEmail = async(req, res) => {
         res.clearCookie('tempToken');
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 24 * 60 * 60 * 1000 // 1 day
         });
 
